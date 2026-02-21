4bd36d60351e1e2075929b2dfebcfdb699384e17
Angel Hernandez <angeldariaux@gmail.com>
2026-02-21 15:42:07 -0600

    added variable block for acm_server_cert_arn

--- modules/appmesh_virtualnode_mtls/main.tf
+++ modules/appmesh_virtualnode_mtls/main.tf
@@ -1,3 +1,28 @@
 resource "aws_appmesh_mesh" "this_mesh" {
   name = var.mesh_name
 }
+
+resource "aws_appmesh_virtual_node" "api_customer" {
+    name = "api-customer"
+    mesh_name = aws_appmesh_mesh.this_mesh.name
+
+    spec {
+      listener {
+        port_mapping {
+        port     = var.listener_port
+        protocol = "http"
+      }
+      }
+
+      # TLS para terminación en el Virtual Node usando ACM (requerimiento)
+      tls {
+        mode = "STRICT"
+
+        certificate {
+            acm {
+                certificate_arn = var.acm_server_cert_arn
+            }
+        }
+      }
+    }
+}
--- modules/appmesh_virtualnode_mtls/variables.tf
+++ modules/appmesh_virtualnode_mtls/variables.tf
@@ -2,3 +2,21 @@ variable "mesh_name" {
   type = string
   default = "zigi-mesh"
 }
+
+variable "listener_port" {
+  type    = number
+  default = 8080
+  validation {
+    condition     = var.listener_port >= 1 && var.listener_port <= 65535
+    error_message = "listener_port debe ser un puerto válido."
+  }
+}
+
+variable "acm_server_cert_arn" {
+  type        = string
+  description = "ARN del cert en ACM usado para TLS en el Virtual Node"
+  validation {
+    condition     = can(regex("^arn:aws:acm:", var.acm_server_cert_arn))
+    error_message = "acm_server_cert_arn debe ser un ARN de ACM."
+  }
+}
