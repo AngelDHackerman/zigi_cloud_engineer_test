10ed77dea78aacb9a476a728fe2fdca71581fc5c
Angel Hernandez <angeldariaux@gmail.com>
2026-02-21 08:14:34 -0600

    added a variable for bucket_name, bucket_prefix AND block for iam document policy

--- modules/irsa_s3/main.tf
+++ modules/irsa_s3/main.tf
@@ -7,3 +7,17 @@ locals {
   oidc_issuer_url = data.aws_eks_cluster.this_cluster.identity[0].oidc[0].issuer
   oidc_issuer_hostpath = replace(local.oidc_issuer_url, "https://", "")
 }
+
+# Descubre el proveedor OIDC existente por su URL (sin harcodear el ARN)
+data "aws_iam_openid_connect_provider" "this_cluster" {
+  url = local.oidc_issuer_hostpath
+}
+
+data "aws_iam_policy_document" "s3_min_policy" {
+  statement {
+    sid = "S3ListBucket"
+    effect = "Allow"
+    actions = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${var.bucket_name}"]
+  }
+}
--- modules/irsa_s3/variables.tf
+++ modules/irsa_s3/variables.tf
@@ -6,3 +6,18 @@ variable "eks_cluster_name" {
       error_message = "eks_cluster_name no puede estar vacio"
     }
 }
+
+variable "bucket_name" {
+  type = string
+  description = "Bucket S3 destino"
+  validation {
+    condition = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
+    error_message = "bucket_name debe cumplir naming rules de S3 (3-63, lowercase, digits, . , -)."
+  }
+}
+
+variable "bucket_prefix" {
+  type        = string
+  description = "Prefix dentro del bucket para limitar permisos"
+  default     = "app/"
+}
