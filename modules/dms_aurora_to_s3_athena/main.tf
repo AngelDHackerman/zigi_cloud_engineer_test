213fb9047d99b5b6e49c223d6e2dddc48e9d7305
Angel Hernandez <angeldariaux@gmail.com>
2026-02-21 16:35:01 -0600

    added resource block for athena workgroup and the needed variables

--- modules/dms_aurora_to_s3_athena/main.tf
+++ modules/dms_aurora_to_s3_athena/main.tf
@@ -1,3 +1,16 @@
 resource "aws_s3_bucket" "athena_results" {
   bucket = var.athena_results_bucket
 }
+
+resource "aws_athena_workgroup" "this_dms" {
+  name = var.athena_workgroup_name
+
+  configuration {
+    enforce_workgroup_configuration = true
+    publish_cloudwatch_metrics_enabled = true
+
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.athena_results.bucket}/${var.athena_results_prefix}/"
+    }
+  }
+}
--- modules/dms_aurora_to_s3_athena/variables.tf
+++ modules/dms_aurora_to_s3_athena/variables.tf
@@ -5,3 +5,19 @@ variable "athena_results_bucket" {
     error_message = "athena_results_bucket inválido."
   }
 }
+
+variable "athena_workgroup_name" {
+  type    = string
+  default = "zigi-wg"
+}
+
+variable "athena_results_prefix" {
+  type        = string
+  description = "Prefijo (folder) dentro del bucket para guardar resultados de Athena (ej: athena/results/)"
+  default     = "athena/results/"
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9/_-]*$", var.athena_results_prefix))
+    error_message = "athena_results_prefix solo puede contener letras, números, /, _, -."
+  }
+}
