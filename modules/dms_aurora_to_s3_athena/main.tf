ec3dd236b1de8e8798c3229d81c3d379c3a56db4
Angel Hernandez <angeldariaux@gmail.com>
2026-02-21 20:48:14 -0600

    used terraform fmt for give proper format to the configuration files

--- modules/dms_aurora_to_s3_athena/main.tf
+++ modules/dms_aurora_to_s3_athena/main.tf
@@ -6,7 +6,7 @@ resource "aws_athena_workgroup" "this_dms" {
   name = var.athena_workgroup_name
 
   configuration {
-    enforce_workgroup_configuration = true
+    enforce_workgroup_configuration    = true
     publish_cloudwatch_metrics_enabled = true
 
     result_configuration {
@@ -16,22 +16,22 @@ resource "aws_athena_workgroup" "this_dms" {
 }
 
 resource "aws_dms_replication_instance" "this_dms" {
-  replication_instance_id = var.dms_replication_instance_id
+  replication_instance_id    = var.dms_replication_instance_id
   replication_instance_class = var.dms_instance_class
-  allocated_storage = var.dms_allocated_storage_gb
-  publicly_accessible = false
-  multi_az = true
+  allocated_storage          = var.dms_allocated_storage_gb
+  publicly_accessible        = false
+  multi_az                   = true
 }
 
 resource "aws_dms_endpoint" "source_aurora" {
-  endpoint_id = "${var.name_prefix}-aurora-src"
+  endpoint_id   = "${var.name_prefix}-aurora-src"
   endpoint_type = "source"
-  engine_name = "aurora-postgresql"
+  engine_name   = "aurora-postgresql"
 
-  server_name = var.aurora_endpoint
-  port = var.aurora_port
-  username = var.aurora_username
-  password = var.aurora_password
+  server_name   = var.aurora_endpoint
+  port          = var.aurora_port
+  username      = var.aurora_username
+  password      = var.aurora_password
   database_name = var.aurora_db_name
 
   ssl_mode = "require"
@@ -61,26 +61,26 @@ locals {
   # Solo schema public y tablas trx_*
   table_mappings = jsonencode({
     rules = [
-        {
-            "rule-type"      = "selection",
-            "rule-id"        = "1",
-            "rule-name"      = "public_trx_only",
-            "object-locator" = {
-                "schema-name" = "public",
-                "table-name"  = "trx_%"
-            },
-            "rule-action"    = "include"
-        }
+      {
+        "rule-type" = "selection",
+        "rule-id"   = "1",
+        "rule-name" = "public_trx_only",
+        "object-locator" = {
+          "schema-name" = "public",
+          "table-name"  = "trx_%"
+        },
+        "rule-action" = "include"
+      }
     ]
   })
 }
 
 resource "aws_dms_replication_task" "this_dms" {
-  replication_task_id = var.dms_task_id
-  migration_type = "full_load-and-cdc"
+  replication_task_id      = var.dms_task_id
+  migration_type           = "full_load-and-cdc"
   replication_instance_arn = aws_dms_replication_instance.this_dms.replication_instance_arn
-  source_endpoint_arn = aws_dms_endpoint.source_aurora.endpoint_arn
-  target_endpoint_arn = aws_dms_endpoint.target_s3.endpoint_arn
+  source_endpoint_arn      = aws_dms_endpoint.source_aurora.endpoint_arn
+  target_endpoint_arn      = aws_dms_endpoint.target_s3.endpoint_arn
 
   table_mappings = local.table_mappings
 }
