79e9b0bdafbf0f7221dde7987ecacaf0d3b76fc9
Angel Hernandez <angeldariaux@gmail.com>
2026-02-22 08:09:57 -0600

    added code in dev/variables.tf for match root and child variables in modules

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -40,3 +40,15 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
+
+# Module 1: IRSA + S3 policy + ServiceAccount
+module "irsa_s3" {
+  source = "../../modules/irsa_s3"
+
+  eks_cluster_name      = var.eks_cluster_name
+  bucket_name           = var.irsa_bucket_name
+  bucket_prefix         = var.irsa_bucket_prefix
+  k8s_namespace         = var.k8s_namespace
+  service_account_name  = var.service_account_name
+  name_prefix           = var.name_prefix
+}
--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -3,5 +3,28 @@ variable "aws_region" {
     default = "us-east-1" 
 }
 
+variable "name_prefix" { 
+    type = string 
+    default = "zigi-dev" 
+}
+
 # IRSA/EKS
-variable "eks_cluster_name" { type = string }
+variable "eks_cluster_name" { 
+    type = string 
+}
+variable "irsa_bucket_name" { 
+    type = string 
+}
+variable "irsa_bucket_prefix" { 
+    type = string 
+    default = "app/" 
+}
+variable "k8s_namespace" { 
+    type = string 
+    default = "default" 
+}
+variable "service_account_name" { 
+    type = string 
+    default = "api-customer" 
+}
+
